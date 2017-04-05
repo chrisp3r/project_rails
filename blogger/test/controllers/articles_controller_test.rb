f21604e9a448bc47bc380854183f80e48fdb4328
gp2015 <georgec.perez@gmail.com>
2017-04-04 21:16:38 -0400

    Added WIP tests, added articles.destroy and delete button

--- blogger/test/controllers/articles_controller_test.rb
+++ blogger/test/controllers/articles_controller_test.rb
@@ -4,4 +4,24 @@ class ArticlesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should get index" do
+    get :index, :id => 1
+    assert_response :success
+  end
+
+  test "should get show article" do
+    get :show, :id => 1
+    assert_response :success
+  end
+
+  test "should get new article" do
+    #get :new, :id => 1
+    assert_response :success
+  end
+
+  test "should post create article" do
+    #post :create, @article[:title] => "test"
+    assert_response :success
+  end
 end
