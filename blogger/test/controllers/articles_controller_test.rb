ab510869ee2ab13982127f12149858ab54599e5a
gp2015 <georgec.perez@gmail.com>
2017-04-06 09:43:09 -0400

    Added tests from guides.rubyonrails.org/testing.html

--- blogger/test/controllers/articles_controller_test.rb
+++ blogger/test/controllers/articles_controller_test.rb
@@ -6,22 +6,43 @@ class ArticlesControllerTest < ActionController::TestCase
   # end
 
   test "should get index" do
-    get :index, :id => 1
-    assert_response :success
-  end
-
-  test "should get show article" do
-    get :show, :id => 1
+    get :index
     assert_response :success
   end
 
   test "should get new article" do
-    #get :new, :id => 1
+    get :new
+    assert_response :success
+  end
+
+  test "should show article" do
+    article = articles(:one)
+    get article_url(article)
     assert_response :success
   end
 
   test "should post create article" do
     #post :create, @article[:title] => "test"
     assert_response :success
+  end
+
+  test "should destroy article" do
+    article = articles(:one)
+    assert_difference('Article.count', -1) do
+      delete article_url(article)
+    end
+    
+    assert_redirected_to articles_path
+  end
+
+  test "should update article" do
+    article = articles(:one)
+
+    patch article_url(article), params: { article: { title: "updated" } }
+
+    assert_redirected_to article_path(article)
+    # Reload association to fetch updated data and assert that title is updated
+    article.reload
+    assert_equal "updated", article.title
   end
 end
