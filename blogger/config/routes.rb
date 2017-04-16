7e62d4b265e9c0f46af4aab1e0c64e99f531ea1c
gp2015 <georgec.perez@gmail.com>
2017-04-15 22:58:19 -0400

    Working tests added

--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -1,11 +1,21 @@
 #Rails.application.routes.draw do
 Blogger::Application.routes.draw do
 
-  root to: 'articles#index'
+  #root 'articles#index'
+  get '/' => 'articles#index'
+  
   #resources :articles
   resources :articles do
     resources :comments
   end
   resources :tags
+
+  get '/create' => 'articles#create'
+  get '/new' => 'articles#new'
+  get '/edit' => 'articles#edit'
+  get '/show' => 'articles#show'
+  patch '/update' => 'articles#update'
+  put '/update' => 'articles#update'
+  delete '/destroy' => 'articles#destroy'
 
 end
--- blogger/test/controllers/articles_controller_test.rb
+++ blogger/test/controllers/articles_controller_test.rb
@@ -5,8 +5,31 @@ class ArticlesControllerTest < ActionController::TestCase
   #   assert true
   # end
 
+  def setup
+    @article = articles(:one)
+  end
+
+  def teardown
+    @article = nil
+  end
+
+  test "should get root" do
+    get :index
+    assert_response :success
+  end
+
   test "should get index" do
     get :index
+    assert_response :success
+  end
+
+  test "should show article" do
+    # article = articles(:one) # what does :one do?
+    # get :show, article: { id: 1 } # 302 response
+
+    # @article = Article.first # uses def setup
+    # not the same as :id => 1 but why?
+    get :show, :id => @article.id
     assert_response :success
   end
 
@@ -16,11 +39,40 @@ class ArticlesControllerTest < ActionController::TestCase
   end
 
   test "should create article" do
-    assert_difference('Article.count') do
-      article articles_path, params: { article: { title: @article.title, body: @article.body } }
+    #assert_difference('Article.count') do
+       #article articles_path, params: { article: { title: @article.title, body: @article.body } }
+    #end
+    #assert_redirect_to article_path(Article.last)
+  
+    # Unclear if this is actually working?
+    assert_difference('Article.count', +1) do # +1 doesn't change test?
+      post :create, article: { body: 'body', title: 'hello world' }
     end
-    assert_redirect_to article_path(Article.last)
+
+    assert_redirected_to article_path(Article.last)
   end
+
+  test "should destroy article" do
+    assert_difference('Article.count', -1) do
+      delete :destroy, :id => @article.id
+    end
+
+    assert_redirected_to articles_path
+  end
+
+  test "should get edit" do
+    get :edit, :id => @article.id
+    assert_response :success
+  end
+
+  test "should update article" do
+    #example
+    #put :update, :id => @post.id, :post => { }
+    #assert_redirected_to post_path(assigns(:post))
+    put :update, :id => @article.id, :article => { title: "updated by test" }
+    assert_redirected_to article_url(assigns(:article)) # just :article doesn't pass :id to url
+  end
+
 end
 
 =begin
